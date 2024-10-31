4b64f1523610e971a69baf7760532fdb3d927b10
Jimmy Ormondroyd <jimmy.ormondroyd@outlook.com>
2024-10-31 12:10:37 +0000

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,9 @@
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://pq7wjeldw5vh472uzsnmka2ini0miyiv.lambda-url.eu-west-2.on.aws/");
+    let response = await fetch("https://mv6hzev3nppdwdxuiu4zv4rrzq0nfdwa.lambda-url.eu-west-2.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+    counter.innerHTML = "Hello World";
 }
 
 updateCounter();
