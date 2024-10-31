46dee5a27cf10437b9fa3d3307a7b8ca43943546
Jimmy Ormondroyd <jimmy.ormondroyd@outlook.com>
2024-10-31 12:17:42 +0000

    adding view count to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://mv6hzev3nppdwdxuiu4zv4rrzq0nfdwa.lambda-url.eu-west-2.on.aws/");
     let data = await response.json();
-    counter.innerHTML = "Hello World";
+    counter.innerHTML = `Views: ${data}`;
 }
 
 updateCounter();
